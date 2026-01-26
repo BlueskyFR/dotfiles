2a7b734db9e46f7518f348314595e23e82783cf1
Hugo Cartigny (BlueskyFR) <hugo.cartigny@gmail.com>
2026-01-26 23:27:40 +0100

    ✨ Add cron + set vim as default editor on servers

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -3,34 +3,43 @@
   lib,
   ...
 }: {
-  imports = [./containers.nix];
+  home-manager.users.hugo = {
+    imports = [./containers.nix];
 
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      AllowTcpForwarding = "yes";
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
-    };
+    # Override the default editor for servers to vim
+    home.sessionVariables.EDITOR = lib.mkForce "vim";
   };
 
-  services.vscode-server = {
-    enable = true;
-    enableFHS = true;
-    extraRuntimeDependencies = with pkgs; [
-      # Classic tools
-      git
-      rustup
-      # Nix
-      nixd
-      nil
-      alejandra
-      # Other required
-      zlib
-      openssl.dev
-      pkg-config
-    ];
+  # Enable the OpenSSH daemon.
+  services = {
+    openssh = {
+      enable = true;
+      settings = {
+        AllowTcpForwarding = "yes";
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+
+    vscode-server = {
+      enable = true;
+      enableFHS = true;
+      extraRuntimeDependencies = with pkgs; [
+        # Classic tools
+        git
+        rustup
+        # Nix
+        nixd
+        nil
+        alejandra
+        # Other required
+        zlib
+        openssl.dev
+        pkg-config
+      ];
+    };
+
+    cron.enable = true;
   };
 
   # Firewall
