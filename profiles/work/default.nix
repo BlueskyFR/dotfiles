2800e271b30cb96a884b03a0afe1d0e6ec0995d9
Hugo Cartigny (BlueskyFR) <hugo.cartigny@gmail.com>
2026-08-17 16:56:34 +0200

    ✨ Work: add lldp tools

--- profiles/work/default.nix
+++ profiles/work/default.nix
@@ -5,4 +5,7 @@
 }: {
   # imports = [];
   home-manager.users.hugo.imports = [./home.nix];
+
+  # Enable LLDP through lldpd and lldpcli (e.g. `sudo lldpcli show neighbors`)
+  services.lldpd.enable = true;
 }
