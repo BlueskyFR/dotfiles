f1fa66ed101f34f37f0ba17d03852e6fc5122af6
Hugo Cartigny (BlueskyFR) <hugo.cartigny@gmail.com>
2026-08-18 17:39:22 +0200

    ✨ zbook: multi-screen + nvidia hw settings

--- hosts/zbook/default.nix
+++ hosts/zbook/default.nix
@@ -13,7 +13,6 @@
     ./nvidia.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackages_latest; # linuxKernel.kernels.linux_6_6
   # boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   home-manager.users.hugo = {
@@ -31,6 +30,65 @@
     };
 
     services.kanshi = {
+      enable = true;
+      settings = [
+        {
+          profile = {
+            name = "pc-only";
+            outputs = [
+              {
+                criteria = "eDP-1";
+                status = "enable";
+                scale = 1.0;
+                mode = "1920x1080@60.02Hz";
+              }
+            ];
+            # Switching back to a single monitor is broken unless `hyprctl monitors all` is ran for some reason...
+            # Issue reported at https://gitlab.freedesktop.org/emersion/kanshi/-/issues/127
+            # exec = ["sleep 3 && hyprctl monitors all"];
+          };
+        }
+        {
+          profile = {
+            name = "docked";
+            outputs = [
+              {
+                criteria = "eDP-1";
+                status = "disable";
+              }
+              {
+                # criteria = "DP-8";
+                # Can also be `A  space-separated string containing the output manufacturer, model and serial number
+                # (e.g. "Foocorp ASDF 1234")`, equivalent to the description field on `hyprctl monitors all` if no
+                # field is unknown (If one of these fields is missing, it needs to be populated with the string
+                # "Unknown" (e.g. "Foocorp ASDF Unknown").)
+                criteria = "Hewlett Packard HP E240 6CM7170460";
+                mode = "1920x1080@60Hz";
+                position = "1280,0";
+                status = "enable";
+              }
+              {
+                # criteria = "DP-9";
+                criteria = "HP Inc. HP E24m G4 CNC3230PMC";
+                mode = "1920x1080@74.97Hz";
+                position = "3200,0";
+                status = "enable";
+              }
+              {
+                # criteria = "DP-10";
+                criteria = "HP Inc. HP E24m G4 CNC3280PQG";
+                mode = "1920x1080@74.97Hz";
+                position = "5120,0";
+                status = "enable";
+              }
+            ];
+          };
+        }
+      ];
+    };
+
+    /*
+       services.kanshi = {
       enable = false;
       settings = [
         {
@@ -52,8 +110,8 @@
             outputs = [
               {
                 criteria = "eDP-1";
-                # status = "disable";
-                status = "enable";
+                status = "disable";
+                # status = "enable";
               }
               {
                 criteria = "DP-9";
@@ -82,24 +140,24 @@
             outputs = [
               {
                 criteria = "eDP-1";
-                # status = "disable";
-                status = "enable";
+                status = "disable";
+                # status = "enable";
               }
               {
                 criteria = "DP-11";
-                mode = "1920x1080@60Hz";
+                mode = "1920x1080@74.97Hz";
                 position = "3840,0";
                 status = "enable";
               }
               {
                 criteria = "DP-10";
-                mode = "1920x1080@60Hz";
+                mode = "1920x1080@60.00Hz";
                 position = "0,0";
                 status = "enable";
               }
               {
                 criteria = "DP-12";
-                mode = "1920x1080@60Hz";
+                mode = "1920x1080@74.97Hz";
                 position = "1920,0";
                 status = "enable";
               }
@@ -108,6 +166,7 @@
         }
       ];
     };
+    */
 
     home.packages = with pkgs; [
       # hyprdynamicmonitors
--- hosts/zbook/nvidia.nix
+++ hosts/zbook/nvidia.nix
@@ -1,54 +1,82 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  # Obtained with `lspci -d ::03xx`
+  iGpuId = "00:02.0";
+  # Name of the written symlink to our igpu
+  iGpuName = "igpu";
+  nvidiaGpuId = "01:00.0";
+  nvidiaGpuName = "nvidia";
+in {
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"];
   # Blacklist nouveau
   boot.blacklistedKernelModules = ["nouveau"];
 
-  hardware.nvidia = {
-    # Modesetting is required.
-    modesetting.enable = true;
+  # Make Hyprland use the integrated GPU:
+  # create a device name from our iGPU's hardware id so we can pass it to Hyprland
+  services.udev.extraRules = ''
+    KERNEL=="card*", KERNELS=="0000:${iGpuId}", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/${iGpuName}"
+    KERNEL=="card*", KERNELS=="0000:${nvidiaGpuId}", SUBSYSTEM=="drm", SUBSYSTEMS=="pci", SYMLINK+="dri/${nvidiaGpuName}"
+  '';
+  environment.sessionVariables.AQ_DRM_DEVICES = "/dev/dri/${nvidiaGpuName}:/dev/dri/${iGpuName}";
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    powerManagement.enable = true;
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+  hardware = {
+    # Enable OpenGL
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [intel-media-driver libvdpau-va-gl];
+    };
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-    open = true;
+    nvidia = {
+      # Modesetting is required.
+      modesetting.enable = true;
 
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-    nvidiaPersistenced = false;
-    # Enable if screen tearing issues, slows down driver startup
-    forceFullCompositionPipeline = false;
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = true;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Do not disable this unless your GPU is unsupported or if you have a good reason to.
+      open = true;
 
-    prime = {
-      # Use GPU by default if true
-      sync.enable = false;
-      # OR enable GPU offload
-      offload = {
-        enable = true;
-        enableOffloadCmd = true; # Provides `nvidia-offload`
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+      nvidiaPersistenced = false;
+      # Enable if screen tearing issues, slows down driver startup
+      forceFullCompositionPipeline = false;
+
+      videoAcceleration = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+      prime = {
+        # Use GPU by default if true
+        sync.enable = false;
+        # OR enable GPU offload
+        offload = {
+          enable = true;
+          enableOffloadCmd = true; # Provides `nvidia-offload`
+        };
+
+        # Make sure to use the correct Bus ID values for your system!
+        # The values are obtained with `nix-shell -p lshw --run "sudo lshw -c display"`
+        # but I feel like `lspci | grep -E 'VGA|3D'` is better
+        # Then format `00:02.0` as "PCI:0:2:0" and `01:00.0` as "PCI:1:0:0"
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
       };
-
-      # Make sure to use the correct Bus ID values for your system!
-      # The values are obtained with `nix-shell -p lshw --run "sudo lshw -c display"`
-      # but I feel like `lspci | grep -E 'VGA|3D'` is better
-      # Then format `00:02.0` as "PCI:0:2:0" and `01:00.0` as "PCI:1:0:0"
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
     };
   };
 }
